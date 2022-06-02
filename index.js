45f9a0223249ad5b6fa6940f787badecea23eb8d
taiwonaf <nafiutaiwosolomon@gmail.com>
2022-06-02 23:49:28 +0100

    Desktop view completed

---
+++ index.js
@@ -0,0 +1,13 @@
+// const company = document.getElementById("company");
+// const features = document.getElementById("features");
+
+
+// company.onmouseover = () => {
+//     const dropPar = company.parentElement
+//     dropPar.classList.toggle("active");
+// }
+
+// features.onmouseover = () => {
+//     const dropPar = features.parentElement
+//     dropPar.classList.toggle("active");
+// }
